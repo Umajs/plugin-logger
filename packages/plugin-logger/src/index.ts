ce3dfeeef85f92feb7681d27479929369e62e02a
BubbleM <bubblelm@163.com>
2020-04-06 13:49:12 +0800

    style:clear

--- packages/plugin-logger/src/index.ts
+++ packages/plugin-logger/src/index.ts
@@ -11,26 +11,9 @@ export default (ursa: Ursa, options: TUrsaLoggerOption) => {
         allowDebugAtProd: true,
         encoding: 'utf-8',
         outputJSON: true,
-        // dir: path.join(ursa.options.ROOT, 'logger'),
-        // errorLogName: 'errorlogger.log',
-        // infoLogName: 'infologger.log',
-        // warnLogName: 'warnlogger.log',
         file: path.join(ursa.options.ROOT, '../logger/logger.log'), // 日志默认和src同级
         ...options,
     });
-    // const loggerOption = {
-    //     level: 'ALL',
-    //     consoleLevel: 'ALL',
-    //     allowDebugAtProd: true,
-    //     encoding: 'utf-8',
-    //     outputJSON: true,
-    //     // dir: path.join(ursa.options.ROOT, 'logger'),
-    //     // errorLogName: 'errorlogger.log',
-    //     // infoLogName: 'infologger.log',
-    //     // warnLogName: 'warnlogger.log',
-    //     file: path.join(ursa.options.ROOT, '../logger/logger.log'), // 日志默认和src同级
-    //     ...options,
-    // };
 
     ursa.app.use((ctx: any, next) => {
         ctx.logger = new ContextLogger(ctx, logger);
